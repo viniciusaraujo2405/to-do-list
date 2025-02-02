e63b6743a93438551c0894ccf1b5e68e7848db03
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-02-02 17:57:21 -0300

    Refatorando a tela de registro

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,79 +1,58 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_list/services/AuthProvider.dart';
 
-class RegisterScreen extends StatefulWidget {
-  @override
-  _RegisterScreenState createState() => _RegisterScreenState();
-}
-
-class _RegisterScreenState extends State<RegisterScreen> {
-  final _formKey = GlobalKey<FormState>();
+class RegisterScreen extends StatelessWidget {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _usernameController = TextEditingController();
 
-  Future<void> _register() async {
-    if (_formKey.currentState!.validate()) {
-      try {
-        UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          
-          email: _emailController.text,
-          password: _passwordController.text,
-          
-        );
-
-         
-
-        Navigator.pushReplacementNamed(context, '/home');
-      } on FirebaseAuthException catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Erro ao registrar: ${e.message}')),
-        );
-      }
-    }
-  }
-
-    @override
+  @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<MyAuthProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('ÁREA DE REGISTRO'),
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
-        ),
+      ),
       body: Center(
         child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                
-                TextFormField(
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(labelText: 'Nome de Usuário'),
+                ),
+                TextField(
                   controller: _emailController,
                   decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor, insira seu email';
-                    }
-                    return null;
-                  },
                 ),
-                TextFormField(
+                TextField(
                   controller: _passwordController,
                   decoration: InputDecoration(labelText: 'Senha'),
                   obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor, insira sua senha';
-                    }
-                    return null;
-                  },
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: _register,
+                  onPressed: () async {
+                    try {
+                      await authProvider.signUpWithEmailAndPassword(
+                        _emailController.text,
+                        _passwordController.text,
+                        _usernameController.text,
+                      );
+                      Navigator.pushReplacementNamed(context, '/home');
+                    } on FirebaseAuthException catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Erro ao registrar: ${e.message}')),
+                      );
+                    }
+                  },
                   child: Text('Registrar'),
                 ),
                 TextButton(
@@ -86,7 +65,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
           ),
         ),
-      ),
       ),
     );
   }
