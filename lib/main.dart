2d37da8444b375681aeed5e1b79e71b358c394b2
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-02-02 11:18:24 -0300

    Adicionando a rota da pagina de tarefas no arquivo .main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart'; 
 import 'package:firebase_core/firebase_core.dart'; 
 import 'firebase_options.dart';
+import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart'; 
 void main() async { 
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
+        '/home': (context) => HomeScreen(),
       },
       debugShowCheckedModeBanner: false,
     );
